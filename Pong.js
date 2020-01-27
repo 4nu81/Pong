9ed9dc386f63e2f0eb833da568341a6b66cbc166
Jaaaaaannick <jannick2109gmail.com>
2020-01-27 08:36:40 +0100

    ping

--- Pong.js
+++ Pong.js
@@ -17,7 +17,7 @@ var gameId = null
 var p1direction = 0
 var p2direction = 0
 var pause = true
-
+// var context = canvas.getContext ("2d") ;
 const UP_KEY = 38;
 const DOWN_KEY = 40;
 const W_KEY = 87;
@@ -28,10 +28,12 @@ const P_KEY = 80;
 
 
 function gameLoop () {
+    drawfield ()
+ //   drawfield2 () 
     if (!pause) {
         
     }
-    drawfield ()
+  
 }
     
     
@@ -55,15 +57,30 @@ function directionchange (evt) {
     }
 }
 function drawfield () { 
-    ctx.fillStyle = "black"
+    ctx.fillStyle = "black";
     ctx.fillRect (0,0, canvas.width, canvas.height);
-    ctx.fillStyle = "white"
+    ctx.fillStyle = "white";
     ctx.fillRect (ballx, bally, Ball, Ball)
+    ctx.fillStyle = "blue";
+    ctx.fillRect (p1positionx,p1positiony, p1Kickerwidth, p1Kickerheight)
+    ctx.fillStyle = "crimson";
+    ctx.fillRect (p2positionx,p2positiony, p2Kickerwidth, p2Kickerheight)
     }
-    
+    //function drawfield2() {
+    //context.beginPath();
+    //context.moveTo(0, 0);
+    //context.lineTo (0,1200);
+    //context.strokeStyle = '#7FFF00';
+    //context.stroke();
+    // }
+
 window.onload = function () {
     var canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     document.addEventListener('keydown', directionchange);
     gameId = setInterval(gameLoop, 50);
+    ctx.fillStyle = "blue";
+    ctx.fillRect (p1positionx,p1positiony, p1Kickerwidth, p1Kickerheight)
+    ctx.fillStyle = "crimson";
+    ctx.fillRect (p2positionx,p2positiony, p2Kickerwidth, p2Kickerheight)
 } 
